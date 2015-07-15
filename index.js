9e9cc8618704d1b554610847ae8c06640cebd119
PICRASMA-EXCELSA <okosunuzflorence@gmail.com>
2015-07-15 10:01:18 +0100

    signin and signup working perfectly

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ document.getElementById("btn-signup").addEventListener('click',function() {
 function signIn(){
 document.getElementById("btn-signin").addEventListener('click',function() {
 
-  //set the values eneterd in the fields to appropriate variable names
+  //store the values eneterd in the fields to appropriate variable names
   /* */
 
     var userEmail = document.getElementById('login-username').value;
@@ -51,27 +51,30 @@ document.getElementById("btn-signin").addEventListener('click',function() {
     if(userEmail === "" || passWord === ""){
       document.getElementById('login-alert').className = "alert alert-danger";
       document.getElementById('login-alert').innerHTML = "You just goofed! You left something out, try again"
-    }else if(pwdMatch !== passWord){
-      document.getElementById('message').className = "alert alert-danger";
-      document.getElementById('message').innerHTML = "Sparky, your password didnt match, try again."
     }else{
 
-    // save user data to localStorage
-    /* */
+      /* retrieve email and password from localStorage */      
 
-    var _user = {};
-    _user.userEmail= userEmail;
-    _user.firstName = firstName;
-    _user.lastName = lastName;
-    _user.passWord = passWord;
-    localStorage.setItem( '_user', JSON.stringify(_user) );
-    pageRedirect();
-  }
+      var _user = JSON.parse( localStorage.getItem( '_user') );
+      var _email = _user.userEmail;
+      var _password = _user.passWord;
+
+      /* match enetered email and password with email and password on localStorage*/
+
+      if(userEmail !== _user && passWord !== _password){
+        document.getElementById('login-alert').className = "alert alert-danger";
+        document.getElementById('login-alert').innerHTML = "Invalid credentials, try again"
+      }else{
+
+        /* Let this niggar in lol call pageRedirect*/
+
+        pageRedirect();
+
+      }
+
+    }
 });
 }
-
-
-
 
 /* Redirect*/
 
